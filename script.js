a9c6e81d703aa24e4920ff0802b4233a866d37a5
Stephany carolina <ster.carolinas@hotmail.com>
2023-05-20 13:23:37 -0300

    Adicionando arquivos

---
+++ script.js
@@ -0,0 +1,29 @@
+const button = document.getElementById("degrad-btn");
+
+button.addEventListener("click", () => {
+  console.log("Botão clicado!");
+});
+
+
+
+
+
+/*modal*/
+
+
+  const button = document.getElementById("degrad-btn");
+
+  button.addEventListener("click", () => {
+    $('#instrucoesModal').modal('show');
+  });
+
+
+
+
+
+
+
+
+
+
+  
